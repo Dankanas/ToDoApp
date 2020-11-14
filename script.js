738e204b4fcfc2873e2aca80058e6f6959450a4f
Dankanas <str3lciunas@gmail.com>
2020-11-14 17:31:23 +0200

    5:31

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 
 const itemArray = [];
 let counter = 1;
-const pushButton = () => {
+
+const addItem = () => {
     const inputItem = document.getElementById('inputName').value;
-    const appLabel = document.createElement('label');
-    const appEntryDiv = document.createElement('div');
-    const appEntryCheckbox = document.createElement('input');
-    const appEntryName = document.createElement('span');
-    const appEntryText = document.createTextNode(inputItem);  
-    const appEntryDelete = document.createElement('button');
-
-    appLabel.setAttribute("for", "app_body_entry_checkbox" + counter.toString());
-    appEntryDiv.setAttribute("class", 'app_body_entry');
-    appEntryDiv.setAttribute("id", "app_body_entry" + counter.toString());
-    appEntryCheckbox.setAttribute("type", "checkbox");
-    appEntryCheckbox.setAttribute("class", "app_body_entry_checkbox");
-    appEntryCheckbox.setAttribute("id", "app_body_entry_checkbox" + counter.toString());
-    appEntryName.setAttribute("class", "app_body_entry_name");
-    appEntryDelete.setAttribute("class", "app_body_entry_delete");
-    appEntryDelete.setAttribute("onclick", "deleteElement(app_body_entry" + counter.toString()+")");
-
-    appEntryName.appendChild(appEntryText);
-    appEntryDiv.appendChild(appEntryCheckbox);
-    appEntryDiv.appendChild(appEntryName);
-    appEntryDiv.appendChild(appEntryDelete);
-    appLabel.appendChild(appEntryDiv);
-    const placer = document.getElementById('app_body');
-    placer.appendChild(appLabel);
-    counter++;
-
+    itemArray.push(inputItem);
+    putElement();
 }
+
+const createElement = (idname, index) => {
+    return  `<label for="checkbox${index}">
+    <div class="app_body_entry_outside">
+        <div class="app_body_entry">
+            <input type="checkbox" class="app_body_entry_checkbox" id="checkbox${index}">
+            <div class="newCheckbox"></div>
+            <span class="app_body_entry_name">${idname}</span>
+            <button class="app_body_entry_delete" id="delete${index}" onclick="deleteElement(${index})"></button>
+        </div> 
+    </div></label>`  
+    
+}
+
+const putElement = () => {
+    appBody = document.getElementById('app_body')
+    appBody.innerHTML = "";
+    itemArray.forEach((x, i) => {appBody.innerHTML += createElement(x, i)})
+    
+}
+
 function deleteElement(param) {
-    param.remove();
+    itemArray.splice(param, 1);
+    putElement();
 }
