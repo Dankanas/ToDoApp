6b72ae49faf5067af3dfda7d1420439b6c07e414
Dankanas <str3lciunas@gmail.com>
2020-11-16 20:36:11 +0200

    obj based + redaguojamas entrylist

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 
 const itemArray = [];
-const checkTest = [];
 
-const addItem = () => {  // Imetu nauja list item + priskiriu jam false verte (ale tipo unchecked)
-    let inputItem = document.getElementById('inputName'); 
-    checkTester(); // <- patikrinu pries tai buvusius list itemu checkboxus
-    itemArray.push(inputItem.value);
-    checkTest.push(false);
+const addItem = () => {  // <- 'Add' button funkcija
+    const inputItem = document.getElementById('inputName'); 
+    checkboxChecker(); 
+    entryNameChecker();
+    itemArray.push({
+        itemName: inputItem.value,
+        itemValue: false
+    });
     putElement();
     inputItem.value = "" ;
     
      // <
 }
 
-const checkTester = () => { // <-- jei checkbox checked tai to elemento verte true , jei ne - false
+const checkboxChecker = () => { // <- Pasiema saraso checkboxo vertes. Jei checked = true, jei ne = false.
     itemArray.forEach ((x, i) => {
         if (document.getElementById(`checkbox${i}`).checked) {
-            checkTest[i] = true;}
+            itemArray[i].itemValue = true;}
         else{
-            checkTest[i] = false;
+            itemArray[i].itemValue = false;
             
         }
     })
 }
 
-const createElement = (idname, index, checked) => {
+const entryNameChecker = () => { // <- Pasiema saraso input vertes.
+    itemArray.forEach((x, i) => {x.itemName = document.getElementById(`app_body_entry_name${i}`).value});
+}
+
+const createElement = (idname, index, checked) => {  // <- Sukuria HTML elementa
     return  `<label for="checkbox${index}">
     <div class="app_body_entry_outside">
         <div class="app_body_entry">
             <input type="checkbox" class="app_body_entry_checkbox" id="checkbox${index}" ${checked}>
             <div class="newCheckbox"></div>
-            <span class="app_body_entry_name">${idname}</span>
+            <input type="text" class="app_body_entry_name" value="${idname}" id="app_body_entry_name${index}">
             <button class="app_body_entry_delete" id="delete${index}" onclick="deleteElement(${index})"></button>
         </div> 
     </div></label>`  
@@ -38,27 +44,27 @@ const createElement = (idname, index, checked) => {
 }
 
 const putElement = () => {
-    appBody = document.getElementById('app_body');
+    appBody = document.getElementById('app_body'); // Nauja HTML elementa ideda i HTML koda
     appBody.innerHTML = "";
     itemArray.forEach((x, i) => { 
-        if (checkTest[i]) 
-        {appBody.innerHTML += createElement(x, i, "checked")}
+        if (itemArray[i].itemValue) 
+        {appBody.innerHTML += createElement(x.itemName, i, "checked")}
         else
-        {appBody.innerHTML += createElement(x, i, "")}
+        {appBody.innerHTML += createElement(x.itemName, i, "")}
     })
     
 }
 
-function deleteElement(param) {
-    checkTester();
+function deleteElement(param) { // 'Delete' button funkcija. Istrina eilute ir atnaujina sarasa.
+    entryNameChecker();
+    checkboxChecker();
     itemArray.splice(param, 1);
-    checkTest.splice(param, 1);
     putElement();
 }
 
-const inputItem2 = document.getElementById("inputName");
+const inputItem2 = document.getElementById("inputName"); // Click 'Add' button with Enter
 inputItem2.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
+  if (event.keyCode === 13) { // 13 - Enter 'key'
     event.preventDefault();
     document.getElementById("addButton").click();
   }
